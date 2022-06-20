aa22ef270988948f6ed9c2b935801b521174ade8
b3t0 <wb2k9@yahoo.com>
2022-06-20 09:40:56 -0700

    .

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < 100; i++) {
         ctx.beginPath();
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
         ctx.arc(random(canvas.width), random(canvas.height), random(50), 0, 2 * Math.PI);
         ctx.fill();
     }
